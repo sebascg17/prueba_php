133f1715e93957e9b64e7ce029b30e045543378b
sebascg17 <92632179+sebascg17@users.noreply.github.com>
2022-11-26 19:25:23 -0500

    Update vender_producto.php

--- vender_producto.php
+++ vender_producto.php
@@ -13,6 +13,9 @@
 
         while($row = mysqli_fetch_array($result_producto)){ 
             
+            $precio = $row['precio'];
+            $stock = $row['stock'];
+            $precio_unidad = (float)$precio/$stock;
             ?>
             <div class="col-md-4 col-sm-12">
                 <div class="card text-center">
@@ -21,7 +24,7 @@
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">Precio:</h5>
-                        <p class="card-text">$<?php echo $row['precio'] ?></p>
+                        <p class="card-text">$<?php echo $precio_unidad ?></p>
 
                         <h6 class="card-subtitle">Cantidad disponible</h6>
                         <p id="cantDisp" class="card-text"><?php echo $row['stock'] ?></p>
@@ -43,44 +46,23 @@
     <div class="row" >
         <div class="col-12">
             <?php include("includes/alerta.php") ?>
-            <h5 class="title">Productos registrados</h5>
+            <h5 class="title">Productos Vendidos</h5>
             <hr>
             <table class="table table-bordered">
                 <thead>
                     <th>Producto</th>
-                    <th>Referencia</th>
-                    <th>Precio</th>
-                    <th>Peso</th>
-                    <th>Categoría</th>
-                    <th>Stock</th>
-                    <th>Fecha Creación</th>
-                    <th>Accion</th>
+                    <th>Cantidad vendida</th>
+                    <th>Total Vendido</th>
                     <tbody>
                         <?php
-                        $query = "SELECT * FROM ventas";
+                        $query = "SELECT nombre_producto, referencia, vendidos, valor FROM ventas v INNER JOIN producto p on v.id_producto = p.id";
                         $result_producto = mysqli_query($conn, $query);
 
                         while($row = mysqli_fetch_array($result_producto)){ ?>
                             <tr>
+                                <td><?php echo $row['nombre_producto'] ?></td>
                                 <td><?php echo $row['vendidos'] ?></td>
-                                <td><?php echo $row['referencia'] ?></td>
-                                <td>$<?php echo $row['precio'] ?></td>
-                                <td><?php echo $row['peso'] ?>kg</td>
-                                <td><?php echo $row['descripcion'] ?></td>
-                                <td><?php echo $row['stock'] ?></td>
-                                <td><?php echo $row['fecha_c'] ?></td>
-                                <td class="text-center">
-                                    <a href="metodos/editar.php?id=<?php echo $row['id'] ?>"
-                                    class="btn btn-secondary"
-                                    name="editar_producto">
-                                        <i class="fas fa-solid fa-pen"></i>
-                                    </a>
-                                    <a href="metodos/eliminar.php?id=<?php echo $row['id'] ?>" 
-                                    class="btn btn-danger" 
-                                    name="eliminar_producto">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </a>
-                                </td>
+                                <td>$<?php echo $row['valor'] ?></td>
                             </tr>
 
                         <?php } ?>
