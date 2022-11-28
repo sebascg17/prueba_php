80c9bbad2ce47b1040236651883b3f608cc0a200
sebascg17 <92632179+sebascg17@users.noreply.github.com>
2022-11-28 15:24:17 -0500

    Update pruebaphp.sql

--- database/pruebaphp.sql
+++ database/pruebaphp.sql
@@ -1,7 +1,7 @@
 
 --
-CREATE DATABASE IF NOT EXISTS `pruebaphp` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `pruebaphp`;
+-- Base de datos: `pruebaphp`
+--
 
 -- --------------------------------------------------------
 
@@ -11,7 +11,7 @@ USE `pruebaphp`;
 
 CREATE TABLE `categorias` (
   `idCategoria` int(11) NOT NULL,
-  `descripcion` varchar(300) NOT NULL
+  `descripcion` varchar(300) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -34,23 +34,26 @@ INSERT INTO `categorias` (`idCategoria`, `descripcion`) VALUES
 
 CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
-  `nombre_producto` varchar(200) NOT NULL,
-  `referencia` varchar(200) NOT NULL,
+  `nombre_producto` varchar(200) COLLATE utf8_bin NOT NULL,
+  `referencia` varchar(200) COLLATE utf8_bin NOT NULL,
   `precio` float NOT NULL,
   `peso` decimal(10,0) NOT NULL,
   `categoria` int(11) NOT NULL,
   `stock` int(11) NOT NULL,
-  `fecha_c` timestamp NOT NULL DEFAULT current_timestamp()
+  `estado` int(11) NOT NULL,
+  `fecha_c` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre_producto`, `referencia`, `precio`, `peso`, `categoria`, `stock`, `fecha_c`) VALUES
-(7, 'JUAN VALDEZ', 'JV-1564898', 175035, '90', 1, 50, '2022-11-26 17:25:21'),
-(8, 'COLCAFE 3EN1', 'CCF-59159', 90, '50', 1, 20, '2022-11-26 18:12:59'),
-(9, 'NESCAFE', 'NCF-1548456', 640000, '80', 3, 80, '2022-11-26 18:52:19');
+INSERT INTO `producto` (`id`, `nombre_producto`, `referencia`, `precio`, `peso`, `categoria`, `stock`, `estado`, `fecha_c`) VALUES
+(7, 'JUAN VALDEZ', 'JV-1564898', 10000, '90', 2, 0, 0, '2022-11-26 22:25:21'),
+(8, 'COLCAFE 3EN1', 'CCF-59159', 3000, '50', 2, 0, 0, '2022-11-26 23:12:59'),
+(9, 'NESCAFE', 'NCF-1548456', 5000, '80', 3, 50, 1, '2022-11-26 23:52:19'),
+(10, 'TOSTAO', 'TT-687687687', 8000, '55', 1, 45, 1, '2022-11-28 18:38:13'),
+(11, 'COLCAFE', 'CC-56456', 6500, '1', 1, 0, 0, '2022-11-28 18:48:11');
 
 -- --------------------------------------------------------
 
@@ -70,17 +73,20 @@ CREATE TABLE `ventas` (
 --
 
 INSERT INTO `ventas` (`idVenta`, `id_producto`, `vendidos`, `valor`) VALUES
-(7, 7, 5, 17503.5),
-(8, 8, 10, 45),
-(9, 8, 5, 22.5),
-(10, 9, 40, 320000),
-(11, 8, 22, 99),
-(12, 8, 22, 99),
-(13, 9, 81, 648000),
-(14, 7, 55, 192538),
-(15, 7, 25, 87517.5),
-(16, 7, 55, 192538),
-(17, 8, 22, 99);
+(40, 7, 2, 20000),
+(41, 7, 43, 430000),
+(42, 11, 2, 13000),
+(43, 11, 1, 6500),
+(44, 8, 3, 9000),
+(45, 8, 3, 9000),
+(46, 8, 3, 9000),
+(47, 8, 2, 6000),
+(48, 8, 2, 6000),
+(49, 8, 1, 3000),
+(50, 8, 6, 18000),
+(51, 10, 5, 40000),
+(52, 9, 5, 25000),
+(53, 9, 10, 50000);
 
 --
 -- Índices para tablas volcadas
@@ -114,13 +120,13 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- Restricciones para tablas volcadas
@@ -137,11 +143,6 @@ ALTER TABLE `producto`
 --
 ALTER TABLE `ventas`
   ADD CONSTRAINT `producto_producto_ventas` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
---
--- Base de datos: `test`
---
-CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `test`;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
