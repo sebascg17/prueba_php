036634a4364bc3cb3376955fd0f8592ea3e08345
sebascg17 <92632179+sebascg17@users.noreply.github.com>
2022-11-28 13:45:01 -0500

    Update Consultas.sql

--- database/Consultas.sql
+++ database/Consultas.sql
@@ -1,2 +1,2 @@
 SELECT * FROM producto as t1 where t1.stock=(select max(stock) from producto);
-SELECT * FROM ventas as t1 where t1.vendido=(select max(vendido) from ventas);
+SELECT * FROM ventas as t1 where t1.vendidos=(select max(vendidos) from ventas);
